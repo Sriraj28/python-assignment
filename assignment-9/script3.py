6a6ac23ee675f2a52b2ada8fd89f8a40d8ed4d41
Sriraj28 <konchadasriraj@gmail.com>
2026-03-09 13:40:30 +0530

    assignment 9

---
+++ assignment-9/script3.py
@@ -0,0 +1,12 @@
+def circle(r):
+    return 3.14 * r * r
+
+def rectangle(l, w):
+    return l * w
+
+def triangle(b, h):
+    return 0.5 * b * h
+
+print("Circle Area:", circle(5))
+print("Rectangle Area:", rectangle(4, 6))
+print("Triangle Area:", triangle(3, 8))
